9ba652e23a6f24f18e8bf93f1f5ddf0f542d2288
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2020-07-27 13:36:43 +0200

    Second example has a scale-bar of 50 µm.

--- RunExample2.m
+++ RunExample2.m
@@ -17,11 +17,11 @@ root_folder = 'samples';
 % The segmentation results. Here we use the results from Y. Bellaiche lab
 % analysis software. The results must contain a CELL, FRAME, VERTICES and
 % SIDES structures.
-segmentation_filename   = 'SIA_161210_gfapnlsg_Backup_001.mat';
+segmentation_filename   = 'AdultZebrafishBrainSeg.mat';
 
 % The tissue surface mesh. It must be a mesh mapping the tissue surface, 
 % openedd at the tissue borders.
-mesh_filename           = 'SimplifiedMesh.ply';
+mesh_filename           = 'AdultZebrafishBrainSimplifiedMesh.ply';
 
 % The physical unit of length must be specified (not stored elsewhere).
 units = 'µm';
@@ -48,23 +48,24 @@ dpr = deproj.from_bellaiche( ...
 %% Plot morphological parameters.
 
 close all
+scale_bar_length = 50; % um.
 
 fprintf( 'Plotting the cell sizes.\n' )
-dpr.figure_cell_sizes;
+dpr.figure_cell_sizes( scale_bar_length );
 
 fprintf( 'Plotting the tissue orientation.\n' )
-dpr.figure_tissue_orientation;
+dpr.figure_tissue_orientation( scale_bar_length );
 
 fprintf( 'Plotting the tissue local curvature.\n' )
-dpr.figure_curvatures;
+dpr.figure_curvatures( scale_bar_length );
 
 fprintf( 'Plotting the cell elongation and direction.\n' )
-dpr.figure_cell_elongation;
+dpr.figure_cell_elongation( scale_bar_length );
 
 fprintf( 'Plotting the impact of projection distorsion.\n' )
-dpr.figure_distorsions;
+dpr.figure_distorsions( scale_bar_length );
 
 fprintf( 'Plotting the topology figure.\n' )
-dpr.figure_topology;
+dpr.figure_topology( scale_bar_length );
 
 fprintf( 'Finished.\n' )
